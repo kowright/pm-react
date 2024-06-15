1fe8b7fe7dab8c47e9c55b3263b50ef64204e6e4
Kortney Wright <mithreegals1@yahoo.com>
2024-06-15 17:37:18 -0400

    edited App page to show all tasks from endpoint

--- src/App.js
+++ src/App.js
@@ -9,17 +9,48 @@ function App() {
     React.useEffect(() => {
         fetch("/api")
             .then((res) => res.json())
-            .then((data) => setData(data.message));
+            .then((data) => setData(data));
     }, [])
+
+    if (!data) {
+        return <p>Loading...!</p>; // Render loading until data is fetched
+    }
 
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>{!data ? "Loading...!" : data}</p>
+                <h1>Task Test Table View</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Duration</th>
+                            <th>Roadmap</th>
+                            <th>Assignee</th>
+                            <th>Start Date</th>
+                            <th>End Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.message.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.name}</td>
+                                <td>{item.description}</td>
+                                <td>{item.duration}</td>
+                                <td>{item.roadmap}</td>
+                                <td>{item.assignee}</td>
+                                <td>{item.startDate}</td>
+                                <td>{item.endDate}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </header>
         </div>
     );
 }
+
 
 export default App;
